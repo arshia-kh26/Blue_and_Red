f18e3dca47bdfa9e7c032aab9c08a0fb278c6749
Arshia <khosraviarshiaamir@gmail.com>
2024-10-03 00:43:18 -0700

    Initial commit

---
+++ Red_&_blue_project/XO/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace XO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.webHelpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(56, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 63);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(144, 60);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(73, 63);
+            this.button2.TabIndex = 0;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(234, 60);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(73, 63);
+            this.button3.TabIndex = 0;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(56, 219);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(73, 63);
+            this.button4.TabIndex = 0;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(144, 219);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(73, 63);
+            this.button5.TabIndex = 0;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(234, 219);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(73, 63);
+            this.button6.TabIndex = 0;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(56, 138);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(73, 63);
+            this.button7.TabIndex = 0;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(144, 138);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(73, 63);
+            this.button8.TabIndex = 0;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(234, 138);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(73, 63);
+            this.button9.TabIndex = 0;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(369, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resetToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.webHelpToolStripMenuItem});
+            this.helpToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // webHelpToolStripMenuItem
+            // 
+            this.webHelpToolStripMenuItem.Name = "webHelpToolStripMenuItem";
+            this.webHelpToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.webHelpToolStripMenuItem.Text = "❔ web help";
+            this.webHelpToolStripMenuItem.Click += new System.EventHandler(this.webHelpToolStripMenuItem_Click);
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.resetToolStripMenuItem.Text = "reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.richTextBox1.Location = new System.Drawing.Point(98, 138);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Horizontal;
+            this.richTextBox1.Size = new System.Drawing.Size(186, 55);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = " Blue win";
+            this.richTextBox1.Visible = false;
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Enabled = false;
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.richTextBox2.Location = new System.Drawing.Point(99, 138);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Horizontal;
+            this.richTextBox2.Size = new System.Drawing.Size(186, 55);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = " Red win";
+            this.richTextBox2.Visible = false;
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Enabled = false;
+            this.richTextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.richTextBox3.Location = new System.Drawing.Point(111, 138);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Horizontal;
+            this.richTextBox3.Size = new System.Drawing.Size(162, 55);
+            this.richTextBox3.TabIndex = 5;
+            this.richTextBox3.Text = "  equal";
+            this.richTextBox3.Visible = false;
+            this.richTextBox3.TextChanged += new System.EventHandler(this.richTextBox3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(369, 322);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Blue & Red ";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem webHelpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+    }
+}
+
---
+++ Red_&_blue_project/XO/Form1.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XO
+{
+    public partial class Form1 : Form
+    {
+        bool a1, a2, a3, a4, a5, a6, a7, a8, a9;
+        bool b1, b2, b3, b4, b5, b6, b7, b8, b9;
+        int a = 0;
+        int b = 0;
+        
+
+
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a++;
+            b = a % 2;
+            if (b == 0)
+            {
+                button1.BackColor = Color.Red;
+                b1 = true;
+            }
+            else {
+                button1.BackColor = Color.Blue;
+                a1 = true;
+            }
+            button1.Enabled = false;
+            if ((a1 && a2 && a3) || (a4 && a5 && a6) || (a7 && a8 && a9) || (a1 && a8 && a6)
+    || (a1 && a8 && a6) || (a4 && a8 && a3) || (a1 && a7 && a4)
+     || (a2 && a8 && a5) || (a3 && a9 && a6))
+            {
+                richTextBox1.Visible = true;
+            }
+            if ((b1 && b2 && b3) || (b4 && b5 && b6) || (b7 && b8 && b9) || (b1 && b8 && b6)
+            || (b1 && b8 && b6) || (b4 && b8 && b3) || (b1 && b7 && b4)
+            || (b2 && b8 && b5) || (b3 && b9 && b6))
+            {
+                richTextBox2.Visible = true;
+            }
+            if (a == 9)
+            {
+                richTextBox3.Visible = true;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            a++;
+            b = a % 2;
+            if (b == 0)
+            {
+                button2.BackColor = Color.Red;
+                b2 = true;
+            }
+            else
+            {
+                button2.BackColor = Color.Blue;
+                a2 = true;
+            }
+            button2.Enabled = false;
+            if ((a1 && a2 && a3) || (a4 && a5 && a6) || (a7 && a8 && a9) || (a1 && a8 && a6)
+    || (a1 && a8 && a6) || (a4 && a8 && a3) || (a1 && a7 && a4)
+     || (a2 && a8 && a5) || (a3 && a9 && a6))
+            {
+                richTextBox1.Visible = true;
+            }
+            if ((b1 && b2 && b3) || (b4 && b5 && b6) || (b7 && b8 && b9) || (b1 && b8 && b6)
+            || (b1 && b8 && b6) || (b4 && b8 && b3) || (b1 && b7 && b4)
+            || (b2 && b8 && b5) || (b3 && b9 && b6))
+            {
+                richTextBox2.Visible = true;
+            }
+            if (a == 9)
+            {
+                richTextBox3.Visible = true;
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            a++;
+            b = a % 2;
+            if (b == 0)
+            {
+                button3.BackColor = Color.Red;
+                b3 = true;
+            }
+            else
+            {
+                button3.BackColor = Color.Blue;
+                a3 = true;
+            }
+            button3.Enabled = false;
+            if ((a1 && a2 && a3) || (a4 && a5 && a6) || (a7 && a8 && a9) || (a1 && a8 && a6)
+    || (a1 && a8 && a6) || (a4 && a8 && a3) || (a1 && a7 && a4)
+     || (a2 && a8 && a5) || (a3 && a9 && a6))
+            {
+                richTextBox1.Visible = true;
+            }
+            if ((b1 && b2 && b3) || (b4 && b5 && b6) || (b7 && b8 && b9) || (b1 && b8 && b6)
+            || (b1 && b8 && b6) || (b4 && b8 && b3) || (b1 && b7 && b4)
+            || (b2 && b8 && b5) || (b3 && b9 && b6))
+            {
+                richTextBox2.Visible = true;
+            }
+            if (a == 9)
+            {
+                richTextBox3.Visible = true;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+            a++;
+            b = a % 2;
+            if (b == 0)
+            {
+                button7.BackColor = Color.Red;
+                b7 = true;
+            }
+            else
+            {
+                button7.BackColor = Color.Blue;
+                a7 = true;
+            }
+            button7.Enabled = false;
+            if ((a1 && a2 && a3) || (a4 && a5 && a6) || (a7 && a8 && a9) || (a1 && a8 && a6)
+    || (a1 && a8 && a6) || (a4 && a8 && a3) || (a1 && a7 && a4)
+     || (a2 && a8 && a5) || (a3 && a9 && a6))
+            {
+                richTextBox1.Visible = true;
+            }
+            if ((b1 && b2 && b3) || (b4 && b5 && b6) || (b7 && b8 && b9) || (b1 && b8 && b6)
+            || (b1 && b8 && b6) || (b4 && b8 && b3) || (b1 && b7 && b4)
+            || (b2 && b8 && b5) || (b3 && b9 && b6))
+            {
+                richTextBox2.Visible = true;
+            }
+            if (a == 9)
+            {
+                richTextBox3.Visible = true;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+            a++;
+            b = a % 2;
+            if (b == 0)
+            {
+                button8.BackColor = Color.Red;
+                b8 = true;
+            }
+            else
+            {
+                button8.BackColor = Color.Blue;
+                a8 = true;
+            }
+            button8.Enabled = false;
+            if ((a1 && a2 && a3) || (a4 && a5 && a6) || (a7 && a8 && a9) || (a1 && a8 && a6)
+    || (a1 && a8 && a6) || (a4 && a8 && a3) || (a1 && a7 && a4)
+     || (a2 && a8 && a5) || (a3 && a9 && a6))
+            {
+                richTextBox1.Visible = true;
+            }
+            if ((b1 && b2 && b3) || (b4 && b5 && b6) || (b7 && b8 && b9) || (b1 && b8 && b6)
+            || (b1 && b8 && b6) || (b4 && b8 && b3) || (b1 && b7 && b4)
+            || (b2 && b8 && b5) || (b3 && b9 && b6))
+            {
+                richTextBox2.Visible = true;
+            }
+            if (a == 9)
+            {
+                richTextBox3.Visible = true;
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+
+            a++;
+            b = a % 2;
+            if (b == 0)
+            {
+                button9.BackColor = Color.Red;
+                b9 = true;
+            }
+            else
+            {
+                button9.BackColor = Color.Blue;
+                a9 = true;
+            }
+            button9.Enabled = false;
+            if ((a1 && a2 && a3) || (a4 && a5 && a6) || (a7 && a8 && a9) || (a1 && a8 && a6)
+    || (a1 && a8 && a6) || (a4 && a8 && a3) || (a1 && a7 && a4)
+     || (a2 && a8 && a5) || (a3 && a9 && a6))
+            {
+                richTextBox1.Visible = true;
+            }
+            if ((b1 && b2 && b3) || (b4 && b5 && b6) || (b7 && b8 && b9) || (b1 && b8 && b6)
+            || (b1 && b8 && b6) || (b4 && b8 && b3) || (b1 && b7 && b4)
+            || (b2 && b8 && b5) || (b3 && b9 && b6))
+            {
+                richTextBox2.Visible = true;
+            }
+            if (a == 9)
+            {
+                richTextBox3.Visible = true;
+            }
+        }
+            
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            a++;
+            b = a % 2;
+            if (b == 0)
+            {
+                button4.BackColor = Color.Red;
+                b4 = true;
+            }
+            else
+            {
+                button4.BackColor = Color.Blue;
+                a4 = true;
+            }
+            button4.Enabled = false;
+            if ((a1 && a2 && a3) || (a4 && a5 && a6) || (a7 && a8 && a9) || (a1 && a8 && a6)
+    || (a1 && a8 && a6) || (a4 && a8 && a3) || (a1 && a7 && a4)
+     || (a2 && a8 && a5) || (a3 && a9 && a6))
+            {
+                richTextBox1.Visible = true;
+            }
+            if ((b1 && b2 && b3) || (b4 && b5 && b6) || (b7 && b8 && b9) || (b1 && b8 && b6)
+            || (b1 && b8 && b6) || (b4 && b8 && b3) || (b1 && b7 && b4)
+            || (b2 && b8 && b5) || (b3 && b9 && b6))
+            {
+                richTextBox2.Visible = true;
+            }
+            if (a == 9)
+            {
+                richTextBox3.Visible = true;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            a++;
+            b = a % 2;
+            if (b == 0)
+            {
+                button5.BackColor = Color.Red;
+                b5 = true;
+            }
+            else
+            {
+                button5.BackColor = Color.Blue;
+                a5 = true;
+            }
+            button5.Enabled = false;
+            if ((a1 && a2 && a3) || (a4 && a5 && a6) || (a7 && a8 && a9) || (a1 && a8 && a6)
+    || (a1 && a8 && a6) || (a4 && a8 && a3) || (a1 && a7 && a4)
+     || (a2 && a8 && a5) || (a3 && a9 && a6))
+            {
+                richTextBox1.Visible = true;
+            }
+            if ((b1 && b2 && b3) || (b4 && b5 && b6) || (b7 && b8 && b9) || (b1 && b8 && b6)
+            || (b1 && b8 && b6) || (b4 && b8 && b3) || (b1 && b7 && b4)
+            || (b2 && b8 && b5) || (b3 && b9 && b6))
+            {
+                richTextBox2.Visible = true;
+            }
+            if (a == 9)
+            {
+                richTextBox3.Visible = true;
+            }
+            
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            a++;
+            b = a % 2;
+            if (b == 0)
+            {
+                button6.BackColor = Color.Red;
+                b6 = true;
+            }
+            else
+            {
+                button6.BackColor = Color.Blue;
+                a6 = true;
+            }
+            button6.Enabled = false;
+            if ((a1 && a2 && a3) || (a4 && a5 && a6) || (a7 && a8 && a9) || (a1 && a8 && a6)
+                || (a1 && a8 && a6) || (a4 && a8 && a3) || (a1 && a7 && a4)
+                 || (a2 && a8 && a5) || (a3 && a9 && a6))
+            {
+                richTextBox1.Visible = true;
+            }
+            if ((b1 && b2 && b3) || (b4 && b5 && b6) || (b7 && b8 && b9) || (b1 && b8 && b6)
+            || (b1 && b8 && b6) || (b4 && b8 && b3) || (b1 && b7 && b4)
+            || (b2 && b8 && b5) || (b3 && b9 && b6))
+            {
+                richTextBox2.Visible = true;
+            }
+            if (a == 9)
+            {
+                richTextBox3.Visible = true;                
+            }
+        }
+
+        private void webHelpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.google.com/");
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+            Environment.Exit(0);
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void richTextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
